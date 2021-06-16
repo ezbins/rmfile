5f26edc1377f267ddfbfd74e458f1286a7fdbf03
dennis <ty3627@gmail.com>
2021-06-16 11:48:54 +0800

    *加入正規表示式,判斷讀檔內容,開頭的檔案路徑

--- rmfile.py
+++ rmfile.py
@@ -1,38 +1,48 @@
 #!/usr/bin/python3
 # coding=UTF-8
 
-import shutil
 import os
+import re
 import datetime
 import time
+import fnmatch
 
 
 def get_datetime():
     return datetime.date.today()
 
 
+def get_removeday():
+    with open("/home/administrator/day.txt", "r", encoding="UTF-8") as days:
+        day = int(days.read().strip())
+        return day
+
+
 try:
-    with open("C:\\Users\\Dennis\\backupfile.txt", "r",
+    with open("/home/administrator/backupfile.txt", "r",
               encoding="UTF-8") as file_path:
         # 取得當下時間，timestamp
         right_date = get_datetime()
         for path in file_path:
-            # 切換目錄
-            os.chdir(path)
-            # 撈目錄底下的檔案們
-            file_list = os.listdir()
-            # 每個檔案取得mtime，與當下時間比較
-            for file in file_list:
-                file_date = datetime.date.fromisoformat(
-                    time.strftime('%Y-%m-%d',
-                                  time.localtime(os.path.getmtime(file))))                
-                if (right_date - file_date).days == 0:
-                    print("The same date")
-
-                # 3天以外刪除
+            patten = re.compile("^\/")
+            if patten.match(path)
+                # 切換目錄
+                os.chdir(path.strip())
+                # 撈目錄底下的檔案們
+                file_list = os.listdir()
+                # 每個檔案取得mtime，與當下時間比較
+                for file in file_list:
+                    if fnmatch.fnmatch(file, "*.bz2"):
+                        file_date = datetime.date.fromisoformat(
+                            time.strftime('%Y-%m-%d',
+                                            time.localtime(os.path.getmtime(file))))
+                        if (right_date - file_date).days >= get_removeday():
+                            os.remove(file)
+                            print("rm down")
 
 except OSError as e:
     print("Error: %s-%s." % (e.FileNotFoundError, e.OSError))
 
 if __name__ == "__main__":
     get_datetime()
+    get_removeday()
